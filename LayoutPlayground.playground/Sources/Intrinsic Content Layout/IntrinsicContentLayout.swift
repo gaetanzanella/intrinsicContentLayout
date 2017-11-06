d8f3b69feb9b7222e03b8882d8f437749aeaeee2
Gaétan Zanella <gaetan.zanella@fabernovel.com>
2017-11-06 17:28:56 +0100

    fixup! Basic layouts

--- LayoutPlayground.playground/Sources/Intrinsic Content Layout/IntrinsicContentLayout.swift
+++ LayoutPlayground.playground/Sources/Intrinsic Content Layout/IntrinsicContentLayout.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+// (gz) inspired by https://developer.apple.com/videos/play/wwdc2016/419/
 public protocol IntrinsicContentLayout {
     func layout(in rect: CGRect)
     func intrinsicSizeThatFits(_ size: CGSize) -> CGSize
